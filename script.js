e0c575c419af9920b679a714fe029852287f12f6
kemenagbayuferdianto-lgtm <kemenag.bayuferdianto@gmail.com>
2026-07-30 11:04:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,14 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbyiWpRPO7c0vqWlJvjOjtMH8RGkAIav9y3wC-_F1zDscQSnHWBt5uaSbZqhNYmDEMdr/exec";
 
 fetch(API_URL)
-.then(res=>res.json())
-.then(data=>{
-
-    let html="";
-
-    data.forEach(kua=>{
-
-        html+=`
-
-        <div class="card">
-
-            <img src="${kua.foto}" alt="${kua.nama}">
-
-            <div class="card-body">
-
-                <span class="status ${kua.status=='AKTIF'?'aktif':'nonaktif'}">
-
-                    ${kua.status}
-
-                </span>
-
-                <h2>${kua.nama}</h2>
-
-                <p><strong>Kepala KUA</strong><br>${kua.kepala}</p>
-
-                <p>📍 ${kua.alamat}</p>
-
-                <p>☎️ ${kua.telepon}</p>
-
-                <p>✉️ ${kua.email}</p>
-
-                <div class="button-group">
-
-                    <a href="${kua.maps}" target="_blank" class="btn maps">
-                        Maps
-                    </a>
-
-                    <a href="${kua.link}" target="_blank" class="btn web">
-                        Website
-                    </a>
-
-                </div>
-
-            </div>
-
-        </div>
-
-        `;
-
-    });
-
-    document.getElementById("cards").innerHTML=html;
-
+.then(res => {
+    console.log("Status:", res.status);
+    console.log("Content-Type:", res.headers.get("content-type"));
+    return res.text();
+})
+.then(text => {
+    console.log(text);
+})
+.catch(err => {
+    console.error(err);
 });
