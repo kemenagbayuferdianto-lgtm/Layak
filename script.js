744ccceee582f894f9e4d8d979fee9e64332c417
kemenagbayuferdianto-lgtm <kemenag.bayuferdianto@gmail.com>
2026-07-30 11:07:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbyiWpRPO7c0vqWlJvjOjtMH8RGkAIav9y3wC-_F1zDscQSnHWBt5uaSbZqhNYmDEMdr/exec";
 
 fetch(API_URL)
-.then(res => {
-    console.log("Status:", res.status);
-    console.log("Content-Type:", res.headers.get("content-type"));
-    return res.text();
+.then(res => res.json())
+.then(data => {
+
+    console.log(data);
+
+    const cards = document.getElementById("cards");
+    console.log(cards); // harus bukan null
+
+    let html = "";
+
+    data.forEach(kua => {
+
+        html += `
+        <div class="card">
+            <img src="${kua.foto}">
+            <div class="card-body">
+                <h2>${kua.nama}</h2>
+            </div>
+        </div>
+        `;
+
+    });
+
+    cards.innerHTML = html;
+
+    console.log(cards.innerHTML);
+
 })
-.then(text => {
-    console.log(text);
-})
-.catch(err => {
-    console.error(err);
-});
+.catch(err => console.error(err));
