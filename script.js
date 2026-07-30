b6f14a6f8d9a351eae1e5a08cd6302ffb3fd502a
Bayu Ferdianto <kemenag.bayuferdianto@gmail.com>
2026-07-30 11:23:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbyiWpRPO7c0vqWlJvjOjtMH8RGkAIav9y3wC-_F1zDscQSnHWBt5uaSbZqhNYmDEMdr/exec";
 
+console.log("Script berhasil dijalankan");
+
 fetch(API_URL)
-.then(res => res.json())
+.then(response => {
+    console.log("Status:", response.status);
+    return response.json();
+})
 .then(data => {
 
-    console.log(data);
+    console.log("Isi data:", data);
+    console.log("Jumlah:", data.length);
 
-    document.getElementById("cards").innerHTML = `
-        <div style="
-            background:green;
-            color:white;
-            padding:30px;
-            font-size:30px;">
-            DATA = ${data.length}
-        </div>
-    `;
+    document.getElementById("cards").innerHTML =
+        `<h1 style="color:red">DATA = ${data.length}</h1>`;
 
 })
-.catch(err=>{
-    console.log(err);
+.catch(error => {
+    console.error("ERROR :", error);
 });
