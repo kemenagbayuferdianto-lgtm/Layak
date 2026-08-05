a459f28b12befc1aa16cb3fcb06fc5f155485270
kemenagbayuferdianto-lgtm <kemenag.bayuferdianto@gmail.com>
2026-08-06 01:40:08 +0700

    Service Worker bunuh diri

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,11 @@
-const CACHE_NAME = "sapa-cache-v1";
-const FILES_TO_CACHE = [
-  "./index.html",
-  "./chat.html",
-  "./style.css",
-  "./script.js",
-  "./assets/logo.png",
-  "./assets/banner.jpg"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)));
-  self.skipWaiting();
-});
+self.addEventListener("install", () => self.skipWaiting());
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(keyList.map((key) => key !== CACHE_NAME && caches.delete(key)))
-    )
+    caches.keys()
+      .then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll())
+      .then((clients) => clients.forEach((c) => c.navigate(c.url)))
   );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
 });
